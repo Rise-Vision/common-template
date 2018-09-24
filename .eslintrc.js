2bbeff3e8f5867d74e6d7cf55e6dd63e5e57eed5
Stuart Lees <stuartlees@gmail.com>
2018-09-24 18:27:33 -0400

    Add config files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "idiomatic"
+  ],
+  "rules": {
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "newline-after-var": [
+      "error",
+      "always"
+    ]
+  },
+  "globals": {
+    "RisePlayerConfiguration": true
+  }
+};
