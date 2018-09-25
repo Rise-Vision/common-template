fe9d2a05f6fe7ed76e2c5139ea4433d301f568e3
Stuart Lees <stuartlees@gmail.com>
2018-09-25 14:57:17 -0400

    Add gulp unit test task

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,47 @@
 /* global require */
 
-( function gulp( console ) {
+( function gulp() {
   "use strict";
 
-  const babel = require("gulp-babel"),
+  const babel = require( "gulp-babel" ),
     gulp = require( "gulp" ),
-    runSequence = require( "run-sequence" );
+    factory = require( "widget-tester" ).gulpTaskFactory,
+    rename = require( "gulp-rename" ),
+    runSequence = require( "run-sequence" ),
+    sourcemaps = require( "gulp-sourcemaps" ),
+    uglify = require( "gulp-uglify" )
 
   gulp.task( "scripts", () => {
     return gulp.src([
       "src/rise-local-messaging.js",
       "src/rise-player-configuration.js"
     ])
-      .pipe( babel( {
+      .pipe( babel({
         presets: [ "env" ],
-
-      } ) )
-      .pipe(gulp.dest("dist"));
-  } );
+      }))
+      .pipe( gulp.dest( "dist" ))
+      .pipe( sourcemaps.init())
+      .pipe( rename(( path ) => {
+        path.basename += ".min";
+      }))
+      .pipe( uglify())
+      .pipe( sourcemaps.write())
+      .pipe( gulp.dest( "dist" ));
+  });
 
   gulp.task( "build", ( cb ) => {
     runSequence( "scripts", cb );
-  } );
+  });
 
-} )( console );
+  gulp.task( "test-unit", factory.testUnitAngular(
+    { testFiles: [
+      "dist/rise-player-configuration.js",
+      "dist/rise-local-messaging.js",
+      "test/unit/*test.js" ] }
+  ));
+
+  gulp.task( "test", [ "build" ], ( cb ) => {
+    runSequence( "test-unit", cb );
+  });
+
+})( console );
