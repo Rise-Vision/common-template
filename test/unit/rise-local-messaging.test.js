05cef7446a661039b3870485526999f0d23bb207
Stuart Lees <stuartlees@gmail.com>
2018-09-26 17:13:42 -0400

    Add initial unit test for LM configure()

--- test/unit/rise-local-messaging.test.js
+++ test/unit/rise-local-messaging.test.js
@@ -1,12 +1,37 @@
-/* global describe, it */
+/* global describe, it, expect */
 
 "use strict";
 
 describe( "configure", function() {
 
+  it( "should not attempt to configure if player type is unsupported", function() {
+    RisePlayerConfiguration.LocalMessaging.configure({ player: "test" });
 
-  it( "should do something", function() {
-    // TODO
+    expect( RisePlayerConfiguration.LocalMessaging.getConnectionType()).to.be.undefined;
+  });
+
+  it( "should not attempt to configure if no connection type provided", function() {
+    RisePlayerConfiguration.LocalMessaging.configure({ player: "electron" });
+
+    expect( RisePlayerConfiguration.LocalMessaging.getConnectionType()).to.be.undefined;
+  });
+
+  it( "should not attempt to configure if connection type not supported", function() {
+    RisePlayerConfiguration.LocalMessaging.configure({ player: "electron", connectionType: "test" });
+
+    expect( RisePlayerConfiguration.LocalMessaging.getConnectionType()).to.be.undefined;
+  });
+
+  it( "should attempt to configure if connection type is 'websocket'", function() {
+    RisePlayerConfiguration.LocalMessaging.configure({ player: "electron", connectionType: "websocket" });
+
+    expect( RisePlayerConfiguration.LocalMessaging.getConnectionType()).to.equal( "websocket" );
+  });
+
+  it( "should attempt to configure if connection type is 'window'", function() {
+    RisePlayerConfiguration.LocalMessaging.configure({ player: "electron", connectionType: "window" });
+
+    expect( RisePlayerConfiguration.LocalMessaging.getConnectionType()).to.equal( "window" );
   });
 
 
