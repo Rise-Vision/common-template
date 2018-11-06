76f61e5cd6a18b982f933866f4e3ae5a2bc5f828
santiagonoguez <santiago@nul-unu.com>
2018-11-06 13:07:21 -0600

    send components ready event on preview

--- src/rise-component-loader.js
+++ src/rise-component-loader.js
@@ -6,16 +6,7 @@ RisePlayerConfiguration.ComponentLoader = (() => {
     if ( event.detail.isConnected ) {
       window.removeEventListener( "rise-local-messaging-connection", connectionHandler );
 
-      Promise.resolve()
-        .then(() => {
-          const detail = { detail: { isLoaded: true } };
-
-          // old event, so we don't break existing pages; will be removed soon
-          window.dispatchEvent( new CustomEvent( "rise-components-loaded", detail ));
-
-          // components ready event, will be moved elsewhere after when this class disappears
-          window.dispatchEvent( new CustomEvent( "rise-components-ready" ));
-        });
+      RisePlayerConfiguration.sendComponentsReadyEvent();
     }
   }
 
