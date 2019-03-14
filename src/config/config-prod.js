430ce0fb9f69c926311401b24cce11ab8815cb54
santiagonoguez <santiago@nul-unu.com>
2019-03-14 09:24:34 -0600

    create function to wto watch attribute data file and build file path

--- src/config/config-prod.js
+++ src/config/config-prod.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 
 const TEMPLATE_COMMON_CONFIG = {
+  GCS_ATTRIBUTE_DATA_FILE: "attribute-data.json",
+  GCS_COMPANY_BUCKET: "risevision-company-notifications",
   LOGGER_CLIENT_ID: "1088527147109-6q1o2vtihn34292pjt4ckhmhck0rk0o7.apps.googleusercontent.com",
   LOGGER_CLIENT_SECRET: "nlZyrcPLg6oEwO9f9Wfn29Wh",
   LOGGER_REFRESH_TOKEN: "1/xzt4kwzE1H7W9VnKB8cAaCx6zb4Es4nKEoqaYHdTD15IgOrJDtdun6zK6XiATCKT",
--- src/rise-watch.js
+++ src/rise-watch.js
@@ -1,5 +1,29 @@
+/* global TEMPLATE_COMMON_CONFIG */
+/* eslint-disable one-var */
+
 RisePlayerConfiguration.Watch = (() => {
 
-  return {};
+  function watchAttributeDataFile() {
+    const companyId = RisePlayerConfiguration.getCompanyId();
+    const presentationId = RisePlayerConfiguration.getPresentationId();
+
+    const filePath = `${
+      TEMPLATE_COMMON_CONFIG.GCS_COMPANY_BUCKET
+    }/${
+      companyId
+    }/template-data/${
+      presentationId
+    }/published/${
+      TEMPLATE_COMMON_CONFIG.GCS_ATTRIBUTE_DATA_FILE
+    }`;
+
+    RisePlayerConfiguration.LocalStorage.watchSingleFile( filePath, () => {
+      // handle response
+    });
+  }
+
+  return {
+    watchAttributeDataFile: watchAttributeDataFile
+  };
 
 })();
