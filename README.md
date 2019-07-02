b1c23ea3907820d0343ecf1ef830b89be6716cf2
Oleg Khasimkhanov <olegrise@users.noreply.github.com>
2019-07-02 09:24:32 -0400

    Send PUD command to Viewer (#66)

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ Common scripts for Rise Templates
 
 ## Build
 
-`
+```
 npm install -g gulp
 npm install
 gulp build
-`
+```
 
 Output scripts will be placed in dist/ directory
 
--- src/rise-play-until-done.js
+++ src/rise-play-until-done.js
@@ -1,21 +1,37 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console, one-var */
 
 RisePlayerConfiguration.PlayUntilDone = (() => {
 
   function reportDone() {
-    if ( !RisePlayerConfiguration.LocalMessaging.isConnected()) {
-      console.log( "Not connected to Local Messaging, cannot report done" );
 
-      return;
-    }
+    if ( _isInViewer()) {
 
-    RisePlayerConfiguration.Helpers.onceClientsAreAvailable( "local-messaging", () => {
-      RisePlayerConfiguration.LocalMessaging.broadcastMessage({
-        topic: "template-done"
+      let message = {
+        topic: "template-done",
+        frameElementId: window.frameElement ? window.frameElement.id : ""
+      }
+
+      window.parent.postMessage( message, "*" );
+
+    } else {
+
+      if ( !RisePlayerConfiguration.LocalMessaging.isConnected()) {
+        console.log( "Not connected to Local Messaging, cannot report done" );
+
+        return;
+      }
+
+      RisePlayerConfiguration.Helpers.onceClientsAreAvailable( "local-messaging", () => {
+        RisePlayerConfiguration.LocalMessaging.broadcastMessage({
+          topic: "template-done"
+        });
       });
-    });
-
+    }
   }
+
+  const _isInViewer = () => {
+    return window.self !== window.top;
+  };
 
   const exposedFunctions = {
     reportDone
