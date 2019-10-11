9600993fa0f759ae889b1d4118a75ac322f0c762
santiagonoguez <santiago@nul-unu.com>
2019-10-11 09:26:06 -0500

    simplify e2e template

--- e2e/sw-precache-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  staticFileGlobs: [ "src/images/*", "shared_bundle_*.js" ],
-  runtimeCaching: [
-    { urlPattern: /^https:\/\/fonts.googleapis.com/, handler: "networkFirst" },
-    { urlPattern: /^https:\/\/widgets.risevision.com/, handler: "networkFirst" }
-  ]
-}
