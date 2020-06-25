75306d9482760ddf60115ac9142c2a9f7f9cf63a
Stepanov <daniil.stepanov@requestum.com>
2020-06-25 20:04:59 +0300

    get caches to clean

--- src/rise-purge-cached-files.js
+++ src/rise-purge-cached-files.js
@@ -2,10 +2,30 @@
 /* eslint-disable no-console, one-var, vars-on-top */
 
 RisePlayerConfiguration.PurgeCacheFiles = (() => {
+  const cacheKeys = [ "rise-image", "rise-video" ];
+
   function purge() {
     return new Promise(( resolve ) => {
-      console.log( "PURGING FILES" );
-      resolve( "done" );
+      return _getCachesNames()
+        .then( cachesNames => {
+          return _getCachesToDelete( cachesNames );
+        })
+        .then( cachesToDelete => {
+          console.log( "cachesToDelete", cachesToDelete );
+          resolve( "done" );
+        })
+    })
+  }
+
+  function _getCachesNames() {
+    return window.caches.keys();
+  }
+
+  function _getCachesToDelete( cachesNames ) {
+    return cachesNames.filter( name => {
+      if ( name.match( cacheKeys [ 0 ]) || name.match( cacheKeys [ 0 ])) {
+        return name;
+      }
     })
   }
 
