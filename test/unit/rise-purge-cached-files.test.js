6941f49870799f098792480325bded2a505ac9c9
Stepanov <daniil.stepanov@requestum.com>
2020-06-26 22:33:52 +0300

    add tests for rise-purge-cached-files

--- test/unit/rise-purge-cached-files.test.js
+++ test/unit/rise-purge-cached-files.test.js
@@ -3,5 +3,39 @@
 
 "use strict";
 
-describe( "PurgeCacheFiles", function() {
+describe("PurgeCacheFiles", function () {
+  var sandbox;
+
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+
+    sandbox.stub(window, "caches").value({
+      keys: function () {
+        return ["rise-image/Value 1", "rise-video/Value 2", "custom/Value 2"];
+      }
+    });
+  })
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it("getCachesNames should return caches keys", function () {
+    console.log(RisePlayerConfiguration.PurgeCacheFiles);
+    var response = RisePlayerConfiguration.PurgeCacheFiles.getCacheNames();
+
+    expect(response).to.deep.equal(["rise-image/Value 1", "rise-video/Value 2", "custom/Value 2"]);
+  });
+
+  it("getComponentCaches should return caches keys to delete", function () {
+    var response = RisePlayerConfiguration.PurgeCacheFiles.getComponentCaches(["rise-image/Value 1", "rise-video/Value 2", "custom/Value 2"]);
+
+    expect(response).to.deep.equal(["rise-image/Value 1", "rise-video/Value 2"]);
+  });
+
+  it("should resolve with 'done' message", function () {
+    RisePlayerConfiguration.PurgeCacheFiles.purge().then(function () {
+      expect(res).to.be("done");
+    });
+  });
 });
